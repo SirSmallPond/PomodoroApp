1c28b27b6ab5c83e743c76aa65e52cc6c3a4099d
SirSmallPond <theosakurai@gmail.com>
2018-03-07 13:23:27 +1100

    added 'assets' and 'src' folders

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ const {app, BrowserWindow} = require('electron')
   
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 600, height: 600})
   
     // and load the index.html of the app.
     win.loadURL(url.format({
-      pathname: path.join(__dirname, '../src/index.html'),
+      pathname: path.join(__dirname, 'src/index.html'),
       protocol: 'file:',
       slashes: true
     }))
